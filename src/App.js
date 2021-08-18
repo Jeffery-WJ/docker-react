e4ee5f169ac920dd1aa0c1b2826b35af758d2160
Jeffery-WJ <elseifj@163.com>
2021-08-18 20:55:06 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        I didn't made it...
+        I made it!
         </p>
         <a
           className="App-link"
